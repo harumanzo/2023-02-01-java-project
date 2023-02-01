6643fd6b1ff94b0030809c81c4c7531fe256e2ee
harumanzo <shuaige0028@gmail.com>
2023-02-01 15:24:35 +0900

    무한루프 내에서 명령어 입력받기

--- src/com/KoreaIT/java/basicAM/Main.java
+++ src/com/KoreaIT/java/basicAM/Main.java
@@ -6,7 +6,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("프로그램 시작");
+		System.out.println("========프로그램 시작========");
 		Scanner sc = new Scanner(System.in);
+		
+		while (true){
+			System.out.printf("명령어 )");
+			String command = sc.nextLine();
+			
+			if (command.equals("system exit")) {
+				break;
+			}
+		}
+		
+		System.out.println("========프로그램 끝========");
+		sc.close();
 	}
 }

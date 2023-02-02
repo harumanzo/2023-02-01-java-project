10e2eb6c1c7c96f8d433eee22e99e789bfca0093
harumanzo <shuaige0028@gmail.com>
2023-02-02 15:22:55 +0900

    게시물 번호와 제목 구현

--- src/com/KoreaIT/java/basicAM/Main.java
+++ src/com/KoreaIT/java/basicAM/Main.java
@@ -1,10 +1,9 @@
 package com.KoreaIT.java.basicAM;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
-
 
 public class Main {
 	public static void main(String[] args) {
@@ -15,17 +14,21 @@ public class Main {
 		
 		while (true){
 			System.out.printf("명령어 )");
-			String command = sc.nextLine();
-			
+			String command = sc.nextLine().trim();
 			
 			if (command.equals("article list")) {
 				if(articles.size() == 0) {
 					System.out.println("게시글이 없습니다.");
 				}
 				else {
-					System.out.println("게시글이 "+ articles.size()+"개 존재합니다.");
+					System.out.println("번호 / 제목");
+					Collections.reverse(articles);
+					for(Article articles2: articles) {
+						System.out.println(articles2.i +"   / "+ articles2.command2);
+					Collections.reverse(articles);
+					}
 				}
-			}
+			}			
 			
 			else if (command.equals("article write")) {
 				System.out.printf("제목 )");
@@ -38,7 +41,8 @@ public class Main {
 				articles.add(article);
 				System.out.println(i+"번글이 생성되었습니다");
 				i++;
-			}
+			}			
+			
 			
 			
 			else if (command.equals("system exit")) {
@@ -56,7 +60,7 @@ public class Main {
 	}
 }
 
-class Article{
+class Article {
 	int i;
 	String command2;
 	String command3;
@@ -65,7 +69,7 @@ class Article{
 		this.i = i;
 		this.command2 = command2;
 		this.command3 = command3;
-		
+
 	}
-	
+
 }

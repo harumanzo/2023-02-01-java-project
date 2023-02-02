05f753055303200904fc486323d1a22c662c02cb
harumanzo <shuaige0028@gmail.com>
2023-02-02 13:42:55 +0900

    게시물 작성기능 구현, 게시물 개수까지 파악

--- src/com/KoreaIT/java/basicAM/Main.java
+++ src/com/KoreaIT/java/basicAM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.basicAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -9,6 +11,7 @@ public class Main {
 		System.out.println("========프로그램 시작========");
 		Scanner sc = new Scanner(System.in);
 		int i = 1;
+		List<Article> articles = new ArrayList<>();
 		
 		while (true){
 			System.out.printf("명령어 )");
@@ -16,7 +19,12 @@ public class Main {
 			
 			
 			if (command.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
+				if(articles.size() == 0) {
+					System.out.println("게시글이 없습니다.");
+				}
+				else {
+					System.out.println("게시글이 "+ articles.size()+"개 존재합니다.");
+				}
 			}
 			
 			else if (command.equals("article write")) {
@@ -25,6 +33,9 @@ public class Main {
 				String command2 = scv.nextLine();
 				System.out.printf("내용 )");
 				String command3 = scv.nextLine();
+				
+				Article article = new Article(i,command2,command3);
+				articles.add(article);
 				System.out.println(i+"번글이 생성되었습니다");
 				i++;
 			}
@@ -43,4 +54,18 @@ public class Main {
 		System.out.println("========프로그램 종료========");
 		sc.close();
 	}
+}
+
+class Article{
+	int i;
+	String command2;
+	String command3;
+
+	public Article(int i, String command2, String command3) {
+		this.i = i;
+		this.command2 = command2;
+		this.command3 = command3;
+		
+	}
+	
 }

931e2a15c7fe462a5df13ce4285a13e2e0a7db65
harumanzo <shuaige0028@gmail.com>
2023-02-07 13:57:16 +0900

    app 코드량 줄이기 및 버그수정

--- src/com/KoreaIT/java/basicAM/App.java
+++ src/com/KoreaIT/java/basicAM/App.java
@@ -8,13 +8,12 @@ import java.util.Scanner;
 
 public class App {
 	static List<Article> articles = new ArrayList<>();
+
 	public void run() {
 		System.out.println("========프로그램 시작========");
 		Scanner sc = new Scanner(System.in);
-	
+
 		maketestdata();
-		
-		
 
 		while (true) {
 
@@ -28,13 +27,14 @@ public class App {
 					System.out.println("번호 /  제목  /  조회수");
 					for (int i2 = articles.size() - 1; i2 >= 0; i2--) {
 						Article articles2 = articles.get(i2);
-						System.out.println(articles2.i + "   / " + articles2.command2+ "  /  " + articles2.조회수);
+						System.out.println(articles2.i + "   / " + articles2.command2 + "  /  " + articles2.조회수);
 					}
 				}
 			}
 
 			else if (command.equals("article write")) {
-				int i = articles.size() + 1;;
+				int i = articles.size() + 1;
+				;
 				System.out.printf("제목 )");
 				Scanner scv = new Scanner(System.in);
 				String command2 = scv.nextLine();
@@ -50,50 +50,24 @@ public class App {
 				i++;
 			}
 
-			else if (command.startsWith("article detail")) {
+			else if (command.startsWith("article")) {
 				String[] arr = command.split(" ");
-				int n = Integer.parseInt(arr[2]);
-				Article foundArticle = null;
-
-				if (articles.size() == 0) {
-					System.out.println(n + "번 게시물은 존재하지 않습니다.");
-				} else {
-					for (int p = 1; p <= articles.size(); p++) {
-						Article article = articles.get(p - 1);
-						if (n == article.i) {
-							foundArticle = article;
-							break;
-						} 
-					}
-				}
-				if (foundArticle ==  null) {
-					System.out.println(n + "번 게시물은 존재하지 않습니다.");
-							continue;
-						}
-				foundArticle.조회수 ++;
-				SimpleDateFormat formatter = new SimpleDateFormat("YYYY-dd-MM HH:mm:ss");
-				System.out.println("번호 : " + foundArticle.i);
-				System.out.println("날짜 : " + formatter.format(foundArticle.now));
-				System.out.println("날짜 : " + formatter.format(foundArticle.update));
-				System.out.println("조회수 : " + foundArticle.조회수);
-				System.out.println("제목 : " + foundArticle.command2);
-				System.out.println("내용 : " + foundArticle.command3);
-			}
-
-			else if (command.startsWith("article delete")) {
-				String[] arr = command.split(" ");
-				int n = Integer.parseInt(arr[2]);
-				Article foundArticle = null;
-				if (articles.size() == 0) {
-					System.out.println(n + "번 게시물은 존재하지 않습니다.");
+				if(arr.length<3) {
+					System.out.println("존재하지 않는 명령어입니다");
 					continue;
-				} else {
-					for (int p = 1; p <= articles.size(); p++) {
-						Article article3 = articles.get(p - 1);
-						if (n == article3.i) {
-							foundArticle = article3;
-							break;
-						} 
+				}else {
+					int n = Integer.parseInt(arr[2]);
+					Article foundArticle = null;
+
+					if (articles.size() == 0) {
+						System.out.println(n + "번 게시물은 존재하지 않습니다.");
+					} else {
+						for (int p = 1; p <= articles.size(); p++) {
+							Article article3 = articles.get(p - 1);
+							if (n == article3.i) {
+								foundArticle = article3;
+								break;
+							}
 
 						}
 					}
@@ -101,51 +75,47 @@ public class App {
 						System.out.println(n + "번 게시물은 존재하지 않습니다.");
 						continue;
 
+					} else {
+						if (arr[1].equals("detail")) {
+							foundArticle.조회수++;
+							SimpleDateFormat formatter = new SimpleDateFormat("YYYY-dd-MM HH:mm:ss");
+							System.out.println("번호 : " + foundArticle.i);
+							System.out.println("날짜 : " + formatter.format(foundArticle.now));
+							System.out.println("날짜 : " + formatter.format(foundArticle.update));
+							System.out.println("조회수 : " + foundArticle.조회수);
+							System.out.println("제목 : " + foundArticle.command2);
+							System.out.println("내용 : " + foundArticle.command3);
+
+						} else if (arr[1].equals("delete")) {
+							for (int p = 1; p <= articles.size(); p++) {
+								Article article3 = articles.get(p - 1);
+								if (n == article3.i) {
+									articles.remove(p - 1);
+									System.out.println(n + "번 게시물이 삭제되었습니다.");
+									break;
+								}
+
 							}
-						
-					
-					for (int p = 1; p <= articles.size(); p++) {
-						Article article3 = articles.get(p - 1);
-						if (n == article3.i) {
-							articles.remove(p - 1);
-							System.out.println(n + "번 게시물이 삭제되었습니다.");
-							break;
+						} else if (arr[1].equals("modify")) {
+							System.out.printf("제목 )");
+							Scanner scv = new Scanner(System.in);
+							String command2 = scv.nextLine();
+							System.out.printf("내용 )");
+							String command3 = scv.nextLine();
+							Date update = new Date();
+
+							foundArticle.command2 = command2;
+							foundArticle.command3 = command3;
+							foundArticle.update = update;
 						}
 
 					}
-				}
-			else if (command.startsWith("article modify")) {
-				String[] arr = command.split(" ");
-				int n = Integer.parseInt(arr[2]);
-				Article foundArticle = null;
-				if (articles.size() == 0) {
-					System.out.println(n + "번 게시물은 존재하지 않습니다.");
-				} else {
-					for (int p = 1; p <= articles.size(); p++) {
-						Article article3 = articles.get(p - 1);
-						if (n == article3.i) {
-							foundArticle = article3;
-							break;
-						} 
-
-						}
-					}
-					if (foundArticle == null) {
-						System.out.println(n + "번 게시물은 존재하지 않습니다.");
-						continue;
-
-							}
-					System.out.printf("제목 )");
-					Scanner scv = new Scanner(System.in);
-					String command2 = scv.nextLine();
-					System.out.printf("내용 )");
-					String command3 = scv.nextLine();
-					Date update = new Date();
 					
-					foundArticle.command2 = command2;
-					foundArticle.command3 = command3;
-					foundArticle.update = update;
 				}
+			
+
+
+			}
 
 			else if (command.equals("system exit")) {
 				break;
@@ -157,16 +127,15 @@ public class App {
 
 		}
 
-	System.out.println("========프로그램 종료========");sc.close();
-}
+		System.out.println("========프로그램 종료========");
+		sc.close();
+	}
 
 	static void maketestdata() {
 		articles.add(new Article(1, new Date(), new Date(), 0, "테스트", "테스트"));
 		articles.add(new Article(2, new Date(), new Date(), 0, "테스트", "테스트"));
 		articles.add(new Article(3, new Date(), new Date(), 0, "테스트", "테스트"));
 		System.out.println("테스트 자료 만들어짐.");
-		
-		
+
 	}
 }
-

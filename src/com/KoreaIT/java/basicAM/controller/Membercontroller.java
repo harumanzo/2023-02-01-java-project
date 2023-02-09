eeb6b0c726e19adebee1b635ff10d0b2c42317cf
harumanzo <shuaige0028@gmail.com>
2023-02-09 20:45:23 +0900

    member delete member detail member modify 구현

--- src/com/KoreaIT/java/basicAM/controller/Membercontroller.java
+++ src/com/KoreaIT/java/basicAM/controller/Membercontroller.java
@@ -1,11 +1,12 @@
 package com.KoreaIT.java.basicAM.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-
+import com.KoreaIT.java.basicAM.dto.Article;
 import com.KoreaIT.java.basicAM.dto.Member;
 
 public class Membercontroller extends Controller {
@@ -13,7 +14,6 @@ public class Membercontroller extends Controller {
 	private Scanner sc;
 	private String command;
 	private String actionMethodName;
-	
 
 	public Membercontroller(List<Member> members, Scanner sc) {
 		this.members = members;
@@ -26,7 +26,7 @@ public class Membercontroller extends Controller {
 		members.add(new Member(3, new Date(), new Date(), "홍학범", "naming", "1234"));
 		System.out.println("테스트 회원 만들어짐.");
 	}
-	
+
 	public static void makeadminaccount() {
 		members.add(new Member(0, new Date(), new Date(), "관리자", "admin", "admin1234"));
 	}
@@ -47,9 +47,17 @@ public class Membercontroller extends Controller {
 		case "logout":
 			dologout();
 			break;
-			
+
 		case "delete":
-			domemberdelete();
+			doadminsomething(command);
+			break;
+
+		case "detail":
+			doadminsomething(command);
+			break;
+
+		case "modify":
+			dosomething(command);
 			break;
 		}
 
@@ -114,17 +122,15 @@ public class Membercontroller extends Controller {
 			break;
 		}
 
-		
-		
 		while (true) {
 			System.out.printf("이름 )");
 			name = scv.nextLine();
-			if(name.equals("관리자")) {
+			if (name.equals("관리자")) {
 				System.out.println("사용 할 수 없는 이름입니다.");
 				continue;
-			}else {
+			} else {
 				break;
-			}	
+			}
 		}
 		Date now = new Date();
 		Date update = new Date();
@@ -223,18 +229,104 @@ public class Membercontroller extends Controller {
 		}
 
 	}
-	
-	public void domemberdelete() {
-		if(loginedmember == null) {
+
+	public void doadminsomething(String command) {
+
+		if (loginedmember == null) {
 			System.out.println("권한이 없습니다!!!");
 			return;
-		}else if(loginedmember.name.equals("admin")) {
-			System.out.println("권한이 있습니다!!!");
-		}else {
+		} else if (loginedmember.membername.equals("admin")) {
+			System.out.println("멤버 관리모드입니다.");
+		} else {
 			System.out.println("권한이 없습니다!!!");
+			return;
+		}
+		String[] arr = command.split(" ");
+
+		if (arr.length < 3) {
+			System.out.println("존재하지 않는 명령어입니다");
+		} else {
+			int n = Integer.parseInt(arr[2]);
+			Member foundMember = null;
+
+			if (members.size() == 0) {
+				System.out.println(n + "번 회원은 존재하지 않습니다.");
+			} else {
+				for (int p = 0; p <= members.size(); p++) {
+					Member member1 = members.get(p);
+					if (n == member1.id) {
+						foundMember = member1;
+						break;
+					}
+
+				}
+			}
+			if (foundMember == null) {
+				System.out.println(n + "번 회원은 존재하지 않습니다.");
+			} else {
+				if (arr[1].equals("detail")) {
+					SimpleDateFormat formatter = new SimpleDateFormat("YYYY-dd-MM HH:mm:ss");
+					System.out.println("회원번호 : " + foundMember.id);
+					System.out.println("가입날짜 : " + formatter.format(foundMember.now));
+					System.out.println("정보 수정날짜 : " + formatter.format(foundMember.update));
+					System.out.println("회원이름 : " + foundMember.name);
+					System.out.println("회원 아이디 : " + foundMember.membername);
+					System.out.println("회원 비밀번호 : " + foundMember.memberpassword);
+
+				} else if (arr[1].equals("delete")) {
+					for (int p = 0; p <= members.size(); p++) {
+						Member member3 = members.get(p);
+						if (n == member3.id) {
+							members.remove(p - 1);
+							System.out.println(n + "번 회원이 삭제되었습니다.");
+							break;
+						}
+
+					}
+				}
+			}
+
+		}
+
+	}
+
+	public void dosomething(String command) {
+		String[] arr = command.split(" ");
+		if (members.size() == 0) {
+			System.out.println("유효한 계정이 존재하지 않습니다!!");
+			return;
+		}
+
+		if (loginedmember == null) {
+			System.out.println("로그인을 먼저 해주십시오!!!");
+		} else {
+			Member foundMember = null;
+			for (int p = 0; p <= members.size(); p++) {
+				Member member1 = members.get(p);
+				if (loginedmember.membername.equals(member1.membername)) {
+					foundMember = member1;
+					break;
+				}
+
+			}
+
+			if (foundMember == null) {
+				System.out.println("잘못된 접근입니다");
+
+			} else if (arr[1].equals("modify")) {
+				System.out.printf("바꾸실 이름 )");
+				Scanner scv = new Scanner(System.in);
+				String newname = scv.nextLine();
+				System.out.printf("바꾸실 비밀번호 )");
+				String newpassword = scv.nextLine();
+				Date update = new Date();
+
+				foundMember.name = newname;
+				foundMember.memberpassword = newpassword;
+				foundMember.update = update;
+				System.out.println("회원님의 정보가 수정되었습니다.");
+			}
+
 		}
 	}
-	
-	
-
 }

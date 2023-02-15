8a89d9d33943f69ea965453101662ca7f68130e7
harumanzo <shuaige0028@gmail.com>
2023-02-15 17:19:06 +0900

    로그인 로그아웃 메뉴 차별화

--- src/com/KoreaIT/java/basicAM/App.java
+++ src/com/KoreaIT/java/basicAM/App.java
@@ -10,7 +10,7 @@ import com.KoreaIT.java.basicAM.controller.Membercontroller;
 import com.KoreaIT.java.basicAM.dto.Article;
 import com.KoreaIT.java.basicAM.dto.Member;
 
-public class App {
+public class App extends Controller{
 	static List<Article> articles = new ArrayList<>();
 	static List<Member> members = new ArrayList<>();
 
@@ -26,57 +26,102 @@ public class App {
 		
 
 		while (true) {
-			if(loginedMember !=null) {
-				System.out.println("1.회원가입   2.로그인   3. 글목록   4. 글 정보");
-			}else {
-				System.out.println("1.로그아웃   2.회원정보   3.회원정보수정   4.회원탈퇴   5.글목록   6.글쓰기");
-			}
+			if(loginedmember !=null) {			
+				System.out.println("1.로그아웃   2.회원정보   3.회원정보수정   4.회원탈퇴   5.글목록   6.글쓰기   7.프로그램 종료");
+				System.out.printf("명령어 )");
+				String command = sc.nextLine().trim();
 
-			System.out.printf("명령어 )");
-			String command = sc.nextLine().trim();
+				if (command.length() == 0) {
+					System.out.println("명령어를 입력해주세요");
+					continue;
+				}
 
-			if (command.length() == 0) {
-				System.out.println("명령어를 입력해주세요");
-				continue;
-			}
+				String[] commandBits = command.split(" "); // article detail 1 / member join
 
-			String[] commandBits = command.split(" "); // article detail 1 / member join
+				if (commandBits.length == 1) {
+					System.out.println("명령어 확인 후 다시 입력해주세요");
+					continue;
+				}
 
-			if (commandBits.length == 1) {
-				System.out.println("명령어 확인 후 다시 입력해주세요");
-				continue;
-			}
+				String controllerName = commandBits[0];
+				String actionMethodName = commandBits[1];
 
-			String controllerName = commandBits[0];
-			String actionMethodName = commandBits[1];
+				Controller controller = null;
 
-			Controller controller = null;
+				if (controllerName.equals("article")) {
+					controller = articlecontroller;
+				} else if (controllerName.equals("member")) {
+					controller = membercontroller;
+				} else {
+					if (controllerName.equals("system")) {
+						if (actionMethodName.equals("exit")) {
+							break;
+						}else {
+							System.out.println("존재하지 않는 명령어 입니다.");
+							continue;
+						}
 
-			if (controllerName.equals("article")) {
-				controller = articlecontroller;
-			} else if (controllerName.equals("member")) {
-				controller = membercontroller;
-			} else {
-				if (controllerName.equals("system")) {
-					if (actionMethodName.equals("exit")) {
-						break;
-					}else {
+					} else {
 						System.out.println("존재하지 않는 명령어 입니다.");
 						continue;
 					}
 
-				} else {
-					System.out.println("존재하지 않는 명령어 입니다.");
+				}
+				controller.doAction(command, actionMethodName);
+			}else {
+				System.out.println("1.회원가입   2.로그인   3. 글목록   4. 글 정보    5.프로그램 종료");
+				System.out.printf("명령어 )");
+				String command = sc.nextLine().trim();
+
+				if (command.length() == 0) {
+					System.out.println("명령어를 입력해주세요");
 					continue;
 				}
 
+				String[] commandBits = command.split(" "); // article detail 1 / member join
+
+				if (commandBits.length == 1) {
+					System.out.println("명령어 확인 후 다시 입력해주세요");
+					continue;
+				}
+
+				String controllerName = commandBits[0];
+				String actionMethodName = commandBits[1];
+
+				Controller controller = null;
+
+				if (controllerName.equals("article")) {
+					controller = articlecontroller;
+				} else if (controllerName.equals("member")) {
+					controller = membercontroller;
+				} else {
+					if (controllerName.equals("system")) {
+						if (actionMethodName.equals("exit")) {
+							break;
+						}else {
+							System.out.println("존재하지 않는 명령어 입니다.");
+							continue;
+						}
+
+					} else {
+						System.out.println("존재하지 않는 명령어 입니다.");
+						continue;
+					}
+
+				}
+				controller.doAction(command, actionMethodName);
 			}
-			controller.doAction(command, actionMethodName);
+
 
 		}
 
 		System.out.println("========프로그램 종료========");
 		sc.close();
+	}
+
+	public void doAction(String command, String actionMethodName) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
